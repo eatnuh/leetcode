05daa7911bc8f7f5a137b37969bff765b18634ba
eatnuh <eatnuh@gamil.com>
2022-04-26 00:34:08 +0900

    Solution 559 - Maximum Depth of N-ary Tree

--- src/test/java/com/eatnuih/Solution559Test.java
+++ src/test/java/com/eatnuih/Solution559Test.java
@@ -1,7 +1,21 @@
 package com.eatnuih;
 
+import com.eatnuih.Solution559.Node;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class Solution559Test {
 
-    Solution559 solution559 = new Solution559();
-    
+    Solution559 solution = new Solution559();
+
+    @Test
+    void testcase1() {
+        Node root = new Node(1, List.of());
+
+        assertEquals(1, solution.maxDepth(root));
+    }
 }
